57e9573184837b773ee75bdb213b8ec565731ab3
Thomas NEMER <thomas.nemer@doctolib.com>
2022-08-10 14:24:42 +0200

    add a non versioned .secrets.sh sourcing in .exports.sh

--- .exports.sh
+++ .exports.sh
@@ -1,6 +1,6 @@
+[ -f $HOME/.secrets.sh ] && source $HOME/.secrets.sh
 export TERM=xterm-256color
 export VISUAL=vim
 export EDITOR=vim
 export PATH=bin:$HOME/bin:/usr/local/bin:$PATH
 export ZSH="$HOME/.oh-my-zsh"
-
