b002d6a0ab7a7bb91a00086f941e3e83c4fff2b7
Thomas NEMER <thomas.nemer@doctolib.com>
2022-10-10 14:49:12 +0200

    vim-bootstrap, alias and exports improvements

--- .alias.sh
+++ .alias.sh
@@ -2,3 +2,4 @@ alias zd=wd
 alias k=kubectl
 alias be="bundle exec"
 alias vi=vim
+alias mine=jetbrains-rubymine
--- .exports.sh
+++ .exports.sh
@@ -2,6 +2,9 @@
 export TERM=xterm-256color
 export VISUAL=vim
 export EDITOR=vim
-export PATH=bin:$HOME/bin:/usr/local/bin:$PATH:$HOME/go/bin
+export PATH=bin:$HOME/go/bin:$HOME/bin:/usr/local/bin:$PATH
 export ZSH="$HOME/.oh-my-zsh"
 export LOADER_CHOICE=esbuild
+export HEADLESS=true
+export GOROOT=$HOME/go
+export GOPATH=$GOROOT/bin
